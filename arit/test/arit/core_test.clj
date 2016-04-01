265136912fe7204fbff55cc5cf27dfc668c7b1ca
squest <sabdaps@gmail.com>
2016-04-02 06:01:01 +0700

    much better tests

--- arit/test/arit/core_test.clj
+++ arit/test/arit/core_test.clj
@@ -3,12 +3,25 @@
     [clojure.test :refer :all]
     [arit.core :refer :all]
     [arit.utils :refer :all]
-    [arit.tracker :refer :all]))
+    [arit.tracker :refer :all]
+    [arit.problems :refer :all]))
 
 (deftest utils
   (testing "Utilities"
     (is (= (cslurp)
            (do (cspit (cslurp))
                (cslurp))))))
+
+(deftest problems
+  (testing "Generator"
+    (let [f (fn [{:keys [a b op bulet]}]
+              (and (number? a)
+                   (number? b)
+                   (fn? op)
+                   (#{true false} bulet)))]
+      (is (= true
+             (every? map? (:funs game))))
+      (is (= true
+             (every? f (:funs game)))))))
 
 

ec8c1cd5f8bedfe43101285aaddca3bd06ab9cb4
squest <sabdaps@gmail.com>
2016-04-02 00:02:12 +0700

    menu

--- arit/src/arit/core.clj
+++ arit/src/arit/core.clj
@@ -4,14 +4,32 @@
     [clj-time.core :as t]
     [clj-time.coerce :as c]))
 
+(declare menu submenu)
+
 (defn -main
   [& args]
-  (print "Choose your Ema .. ")
-  (let [with-ema (read-line)]
-    (condp = with-ema
-      "watson" (println "door")
-      "roberts" (println "yes")
-      "stone" (println "stoned"))))
+  (menu))
+
+(defn menu
+  []
+  (loop []
+    (println "Pick a service : \n 1. Masuk \n 2. Keluar \n 3. Nggak tau \n 0. exit")
+    (let [choice (read-line)]
+      (if (= "0" choice)
+        (println "Thanks for flying with us!")
+        (do (submenu choice)
+            (recur))))))
+
+(defn submenu
+  [ch]
+  (println (str "You're choosing menu number : " ch))
+  (loop []
+    (println "Pick a service : \n 1. Masuk \n 2. Keluar \n 3. Nggak tau \n 0. exit")
+    (let [choice (read-line)]
+      (if (= "0" choice)
+        (println "Thanks for flying with us!")
+        (do (println (str "You're choice is " choice))
+            (recur))))))
 
 
 

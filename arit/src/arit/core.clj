a591d2121776115859e75d653299322e9023886b
squest <sabdaps@gmail.com>
2016-04-02 03:08:22 +0700

    basic functions work

--- arit/src/arit/core.clj
+++ arit/src/arit/core.clj
@@ -2,15 +2,22 @@
   (:gen-class)
   (:require
     [arit.utils :refer :all]
-    [arit.tracker :as tracker :refer [records]]))
+    [arit.tracker :as tracker :refer [records]]
+    [arit.problems :as pro]))
 
 (declare menu play init)
 
 (defn -main [& args]
+  (println "You can start now!")
   (println (with-time)))
 
 (defn save-progress []
   (cspit @records))
+
+(def state (atom {:level (:level @records)
+                  :warning 0
+                  :demote 0
+                  :promote 0}))
 
 (defn menu []
   (loop []
@@ -23,12 +30,30 @@
         (do (save-progress)
             (println "Thanks for flying with us!"))
         (do (condp = choice
-              "1" (play records)
+              "1" (play state records)
               "2" (tracker/reporting (:records @records)))
             (recur))))))
 
 (defn- init []
   (reset! records (cslurp)))
+
+(def ops {- " - " + " + " / " / " * " x "})
+
+(defn display
+  [{:keys [a b op]}]
+  (println (str "Berapa " a (ops op) b))
+  (with-time))
+
+(defn play [state records]
+  (let [{:keys [level warning demote promote]} @state
+        {:keys [records]} @records]
+    (loop []
+      (let [{:keys [a b d op bulet] :as data} (pro/play level)
+            {:keys [answer timing]} (display data)]
+        (cond (= answer "exit")
+              (println "がんばて!!")
+              bulet
+              (cond (== answer (op a b)))))))
 
 
 
--- arit/src/arit/problems.clj
+++ arit/src/arit/problems.clj
@@ -1,1 +1,16 @@
-(ns arit.problems)
+(ns arit.problems
+  (:require
+    [arit.utils :refer :all]))
+
+(def game
+  {:max 0
+   :funs [(fn []
+            (let [b (range 1 10)]
+              {:a 10 :b b :op - :bulet true}))]})
+
+(defn play [level]
+  (let [{:keys [max funs]} game
+        f (if (> level max)
+            (rand-nth funs)
+            (funs level))]
+    (f)))
